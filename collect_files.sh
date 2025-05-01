34e86b2c3bf136fd8e0ad389549b592b50fd9640
Vsevolod <vaokorokov@edu.hse.ru>
2025-05-01 21:38:40 +0300

    done

--- collect_files.sh
+++ collect_files.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-chmod +x "collect_files.sh"
 
 input="$1"
 output="$2"
 
 find "$input" -type f -exec cp {} "$output" \;
+#(Дополнение) Прикладываю материалы, изученные для выполнения задания
+#https://www.uptimia.com/questions/how-to-find-and-copy-files-to-a-single-directory-recursively-in-linux
+#https://www.baeldung.com/linux/find-cp-avoid-warnings
+#https://evmservice.ru/blog/find-v-linux/
